441388d62c7e6e7ca151931c6aaf9863f3fa7c03
shankar <shankar.87.dayal@gmail.com>
2019-07-25 08:48:59 +0530

    To update user name for login

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
@@ -15,7 +15,7 @@ public class JwtInMemoryUserDetailsService implements UserDetailsService {
 	static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
 	static {
-		inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
+		inMemoryUserList.add(new JwtUserDetails(1L, "CapgeminiLBS",
 				"$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
 		inMemoryUserList.add(new JwtUserDetails(2L, "ranga",
 				"$2a$10$IetbreuU5KihCkDB6/r1DOJO0VyU9lSiBcrMDT.biU7FOt2oqZDPm", "ROLE_USER_2"));
