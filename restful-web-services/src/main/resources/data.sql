9bb7d3f9cd464a2b157b524381ffefefb4c23529
shankar <shankar.87.dayal@gmail.com>
2019-08-21 06:06:54 +0530

    To add book along with #StudenId

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 insert into todo(id, username,description,target_date,is_done)
-values(10001, 'CapgeminiLBS', 'Learn JPA', sysdate(), false);
+values(10001, 'CapgeminiLBS', 'Learn JPA #5689', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
-values(10002, 'CapgeminiLBS', 'Learn Data JPA', sysdate(), false);
+values(10002, 'CapgeminiLBS', 'Learn Data Migration #7896', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
-values(10003, 'CapgeminiLBS', 'Learn Microservices', sysdate(), false);
+values(10003, 'CapgeminiLBS', 'Learn Microservices #5432', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
-values(10004, 'CapgeminiLBS', 'Learn Java', sysdate(), true);
+values(10004, 'CapgeminiLBS', 'Learn Java #4567', sysdate(), true);
 
 insert into todo(id, username,description,target_date,is_done)
-values(20005, 'UserLBS', 'Learn Java', sysdate(), true);
+values(20005, 'UserLBS', 'Learn Java 8 #7865', sysdate(), true);
